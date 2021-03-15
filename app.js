9d842d9be357f45a1d21ecb67e77c891f0d523ab
Swahmad <swahmad@dwintl.net>
2021-03-16 04:44:02 +0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Bayern Munich, World Champions!'))
+app.get('/', (req, res) => res.send('Bayern Munich, European Champions for the 7th time!'))
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
